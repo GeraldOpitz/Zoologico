52f234401e51ac807c9ef8560228fc29047a4e1b
GeraldOpitz <geraldopitz@gmail.com>
2023-04-20 10:40:29 -0400

    Botón agregar con campos para agregar datos

--- InterfazPrincipal.java
+++ InterfazPrincipal.java
@@ -51,57 +51,88 @@ public class InterfazPrincipal extends JFrame implements ActionListener {
 
   public void actionPerformed(ActionEvent e) {
     if (e.getSource() == botonAgregarAnimal) {
-        // Acción para agregar un animal
-	JDialog dialogoAgregar = new JDialog(this, "Agregar animal", true);
-	dialogoAgregar.setSize(400, 300);
-	dialogoAgregar.setLocationRelativeTo(null);
+      // Acción para agregar un animal
+      JDialog dialogoAgregar = new JDialog(this, "Agregar animal", true);
+      dialogoAgregar.setSize(400, 300);
+      dialogoAgregar.setLocationRelativeTo(null);
 
-	JLabel labelEspecie = new JLabel("Especie");
-	String[] opciones = {"Elefante", "Jirafa", "Tigre", "Oso polar", "Cebra", "Pinguino", "Leon", "Cocodrilo", "Hiena", "Rinoceronte"};
-	JComboBox<String> comboBoxEspecie = new JComboBox<>(opciones);
+      //Datos de animales
+      JLabel labelEspecie = new JLabel("Especie");
+      String[] animales = {"Elefante", "Jirafa", "Tigre", "Oso polar", "Cebra", "Pinguino", "Leon", "Cocodrilo", "Hiena", "Rinoceronte"};
+      JComboBox<String> comboBoxEspecie = new JComboBox<>(animales);
 
-	JLabel labelNombre = new JLabel("Nombre");
-	JTextField textFieldNombre = new JTextField(20);
+      JLabel labelNombre = new JLabel("Nombre");
+      JTextField textFieldNombre = new JTextField(10);
 
-	// Paneles para el JComboBox y JTextField
-	JPanel panelEspecie = new JPanel(new FlowLayout(FlowLayout.LEFT));
-	panelEspecie.add(labelEspecie);
-	panelEspecie.add(comboBoxEspecie);
-	panelEspecie.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+      JLabel labelEdad = new JLabel("Edad");
+      JTextField textFieldEdad = new JTextField(3);
 
-	JPanel panelNombre = new JPanel(new FlowLayout(FlowLayout.LEFT));
-	panelNombre.add(labelNombre);
-	panelNombre.add(textFieldNombre);
-	panelNombre.setBorder(BorderFactory.createEmptyBorder(0, 5, 5, 5));
+      JLabel labelSexo = new JLabel("Sexo");
+      String[] sexo = {"Macho", "Hembra"};
+      JComboBox<String> comboBoxSexo = new JComboBox<>(sexo);
 
+      JLabel labelSalud = new JLabel("Estado de salud");
+      JTextArea textAreaSalud = new JTextArea(3,20);
+      textAreaSalud.setLineWrap(true);
+      textAreaSalud.setWrapStyleWord(true);
+      JScrollPane scrollPane = new JScrollPane(textAreaSalud);
+
+      // Paneles para los datos
+      JPanel panelEspecie = new JPanel(new FlowLayout(FlowLayout.LEFT));
+      panelEspecie.add(labelEspecie);
+      panelEspecie.add(comboBoxEspecie);
+      panelEspecie.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+      JPanel panelNombre = new JPanel(new FlowLayout(FlowLayout.LEFT));
+      panelNombre.add(labelNombre);
+      panelNombre.add(textFieldNombre);
+      panelNombre.setBorder(BorderFactory.createEmptyBorder(0, 5, 5, 5));
+
+      JPanel panelEdad = new JPanel(new FlowLayout(FlowLayout.LEFT));
+      panelEdad.add(labelEdad);
+      panelEdad.add(textFieldEdad);
+      panelEdad.setBorder(BorderFactory.createEmptyBorder(0, 5, 5, 5));
+
+      JPanel panelSexo = new JPanel(new FlowLayout(FlowLayout.LEFT));
+      panelSexo.add(labelSexo);
+      panelSexo.add(comboBoxSexo);
+      panelSexo.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+      JPanel panelSalud = new JPanel(new FlowLayout(FlowLayout.LEFT));
+      panelSalud.add(labelSalud);
+      panelSalud.add(scrollPane);
+      panelSalud.setBorder(BorderFactory.createEmptyBorder(0, 5, 5, 5));
 	
-	JPanel panelContenedor = new JPanel();
-	panelContenedor.setLayout(new BoxLayout(panelContenedor, BoxLayout.Y_AXIS));
-	panelContenedor.add(panelEspecie);
-	panelContenedor.add(panelNombre);
+      JPanel panelContenedor = new JPanel();
+      panelContenedor.setLayout(new BoxLayout(panelContenedor, BoxLayout.Y_AXIS));
+      panelContenedor.add(panelEspecie);
+      panelContenedor.add(panelNombre);
+      panelContenedor.add(panelEdad);
+      panelContenedor.add(panelSexo);
+      panelContenedor.add(panelSalud);
 
-	// Panel para boton agregar
-	JPanel panelBoton = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-	JButton botonAgregar = new JButton("Agregar");
-	panelBoton.add(botonAgregar);
-	//Boton para agregar y cerrar dialog
-	botonAgregar.addActionListener(new ActionListener() {
-    	public void actionPerformed(ActionEvent e) {
-        dialogoAgregar.dispose();
-    	}
-     });
+      // Panel para boton agregar
+      JPanel panelBoton = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+      JButton botonAgregar = new JButton("Agregar");
+      panelBoton.add(botonAgregar);
+      //Boton para agregar y cerrar dialog
+      botonAgregar.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+      dialogoAgregar.dispose();
+      }
+      });
 
 
-	// Contenedor princiapal
-	JPanel panelPrincipal = new JPanel(new BorderLayout());
-	panelPrincipal.add(panelContenedor, BorderLayout.CENTER);
-	panelPrincipal.add(panelBoton, BorderLayout.SOUTH);
+      // Contenedor principal
+      JPanel panelPrincipal = new JPanel(new BorderLayout());
+      panelPrincipal.add(panelContenedor, BorderLayout.CENTER);
+      panelPrincipal.add(panelBoton, BorderLayout.SOUTH);
 
-	dialogoAgregar.add(panelPrincipal);
-	dialogoAgregar.setVisible(true);
+      dialogoAgregar.add(panelPrincipal);
+      dialogoAgregar.setVisible(true);
 
     } else if (e.getSource() == botonBorrarAnimal) {
-        // Acción para borrar un animal
+    // Acción para borrar un animal
         JOptionPane.showMessageDialog(this, "Funcion borrar animal en construccion.");
     }
   }
