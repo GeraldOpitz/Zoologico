53cb2ed1514448514316c628666cba94da4f8705
GeraldOpitz <geraldopitz@gmail.com>
2023-04-19 23:38:40 -0400

    Boton agregar con combobox,textfield y boton agregar que cierra el JDialog

--- InterfazPrincipal.java
+++ InterfazPrincipal.java
@@ -3,6 +3,8 @@ import java.awt.event.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Component;
 
 public class InterfazPrincipal extends JFrame implements ActionListener {
   private JButton botonAgregarAnimal;
@@ -34,7 +36,7 @@ public class InterfazPrincipal extends JFrame implements ActionListener {
    panelPrincipal.add(botonBorrarAnimal);
 
    // Tabla para los datos
-   String[] columnas = {"Nombre", "Especie", "Edad", "Sexo", "Estado de salud"};
+   String[] columnas = {"Especie", "Nombre", "Edad", "Sexo", "Estado de salud"};
    String[][] datos = {};
    JTable tablaAnimales = new JTable(new DefaultTableModel(datos, columnas));
    tablaAnimales.setPreferredScrollableViewportSize(new Dimension(600, 400));
@@ -49,16 +51,60 @@ public class InterfazPrincipal extends JFrame implements ActionListener {
 
   public void actionPerformed(ActionEvent e) {
     if (e.getSource() == botonAgregarAnimal) {
-      // Acción para agregar un animal
-       JDialog dialogoAgregar = new JDialog(this, "Agregar animal", true);
-       dialogoAgregar.setSize(400, 300);
-       dialogoAgregar.setLocationRelativeTo(null);
-       dialogoAgregar.setVisible(true);
-     } else if (e.getSource() == botonBorrarAnimal) {
-       // Acción para borrar un animal
-          JOptionPane.showMessageDialog(this, "Funcion Borrar animal en construccion.");
-       }
+        // Acción para agregar un animal
+	JDialog dialogoAgregar = new JDialog(this, "Agregar animal", true);
+	dialogoAgregar.setSize(400, 300);
+	dialogoAgregar.setLocationRelativeTo(null);
+
+	JLabel labelEspecie = new JLabel("Especie");
+	String[] opciones = {"Elefante", "Jirafa", "Tigre", "Oso polar", "Cebra", "Pinguino", "Leon", "Cocodrilo", "Hiena", "Rinoceronte"};
+	JComboBox<String> comboBoxEspecie = new JComboBox<>(opciones);
+
+	JLabel labelNombre = new JLabel("Nombre");
+	JTextField textFieldNombre = new JTextField(20);
+
+	// Paneles para el JComboBox y JTextField
+	JPanel panelEspecie = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	panelEspecie.add(labelEspecie);
+	panelEspecie.add(comboBoxEspecie);
+	panelEspecie.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+	JPanel panelNombre = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	panelNombre.add(labelNombre);
+	panelNombre.add(textFieldNombre);
+	panelNombre.setBorder(BorderFactory.createEmptyBorder(0, 5, 5, 5));
+
+	
+	JPanel panelContenedor = new JPanel();
+	panelContenedor.setLayout(new BoxLayout(panelContenedor, BoxLayout.Y_AXIS));
+	panelContenedor.add(panelEspecie);
+	panelContenedor.add(panelNombre);
+
+	// Panel para boton agregar
+	JPanel panelBoton = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+	JButton botonAgregar = new JButton("Agregar");
+	panelBoton.add(botonAgregar);
+	//Boton para agregar y cerrar dialog
+	botonAgregar.addActionListener(new ActionListener() {
+    	public void actionPerformed(ActionEvent e) {
+        dialogoAgregar.dispose();
+    	}
+     });
+
+
+	// Contenedor princiapal
+	JPanel panelPrincipal = new JPanel(new BorderLayout());
+	panelPrincipal.add(panelContenedor, BorderLayout.CENTER);
+	panelPrincipal.add(panelBoton, BorderLayout.SOUTH);
+
+	dialogoAgregar.add(panelPrincipal);
+	dialogoAgregar.setVisible(true);
+
+    } else if (e.getSource() == botonBorrarAnimal) {
+        // Acción para borrar un animal
+        JOptionPane.showMessageDialog(this, "Funcion borrar animal en construccion.");
     }
+  }
 
   public static void main(String[] args) {
     InterfazPrincipal interfaz = new InterfazPrincipal();
@@ -68,4 +114,3 @@ public class InterfazPrincipal extends JFrame implements ActionListener {
     interfaz.setVisible(true);
   }
 }
-
