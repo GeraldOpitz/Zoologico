b61f450541bfa67e59105d5fd5af51bd4206a42e
GeraldOpitz <geraldopitz@gmail.com>
2023-04-19 20:37:09 -0400

    segunda versión de la pantalla bienvenida con elementos centrados e imagen con mejor resolución

--- Bienvenida.java
+++ Bienvenida.java
@@ -3,40 +3,42 @@ import java.awt.event.*;
 import java.awt.*;
 
 public class Bienvenida extends JFrame implements ActionListener{
-  private JTextField textfield1;
-  private JLabel label1, label2, label3, label4;
-  private JButton boton1;
+  private JButton botonAcceder;
+  private JLabel label1;
 
   public Bienvenida(){
+   JPanel panel = new JPanel(new BorderLayout());
+   //titulo de la ventana
+   setTitle("Bienvenida");
    //icono que se muestra en la parte superior del programa
-   setLayout(null);
-   setTitle("Sistema de inventario de zoologico");
    setIconImage(new ImageIcon(getClass().getResource("images/icono.png")).getImage());
 
    //imagen de bienvenida al programa
-   ImageIcon imagen = new ImageIcon(new ImageIcon("images/bienvenido.png").getImage().getScaledInstance(550, 300, Image.SCALE_DEFAULT));
+   ImageIcon imagen = new ImageIcon(new ImageIcon("images/bienvenido.png").getImage().getScaledInstance(700, 300, Image.SCALE_DEFAULT));
    label1 = new JLabel(imagen);
-   label1.setBounds(20,50,550,300);
-   add(label1);
+   label1.setHorizontalAlignment(SwingConstants.CENTER);
+   panel.add(label1, BorderLayout.CENTER);
    
-   //botón de acceso a la siguiente parte del programa
-   boton1 = new JButton("Ingresar");
-   boton1.setBounds(140,450,300,50);
-   boton1.addActionListener(this);
-   add(boton1);
+   //botón de acceso a la siguiente ventana
+   botonAcceder = new JButton("Ingresar");
+   botonAcceder.setFont(new Font("Arial", Font.PLAIN, 40));
+   botonAcceder.addActionListener(this);
+   panel.add(botonAcceder, BorderLayout.SOUTH);
+   add(panel);
   }
 
   //metodo para pasar a la siguiente pantalla
   public void actionPerformed(ActionEvent e){
-   if(e.getSource() == boton1){
+   if(e.getSource() == botonAcceder){
      
    }
   }
   //definicion de la clase main
   public static void main(String args[]){
     Bienvenida ventanabienvenida = new Bienvenida();
-    ventanabienvenida.setBounds(0,0,600,600);
-    ventanabienvenida.setVisible(true);
+    ventanabienvenida.setSize(800, 600);
+    ventanabienvenida.setDefaultCloseOperation(EXIT_ON_CLOSE);
     ventanabienvenida.setLocationRelativeTo(null);
+    ventanabienvenida.setVisible(true);
   }
 }
